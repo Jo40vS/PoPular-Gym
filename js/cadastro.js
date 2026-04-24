2090f9d1dce061e244490361a0d4c94105a9cf90
Jo40vS <j040vs0uz4@gmail.com>
2026-04-24 07:15:36 -0300

    feat: validacao do form de cadastro

--- js/cadastro.js
+++ js/cadastro.js
@@ -4,25 +4,50 @@ const frm = document.querySelector("form");
 frm.addEventListener('submit',(e) => {
     e.preventDefault();
 
-    const nome = frm.inName.value;
-    const email = frm.inEmail.value;
-    const senha = frm.inSenha.value;
-    const confirmaSenha = frm.inConfirmarSenha.value;
+    const nome = document.getElementById("inName").value;
+    const email = document.getElementById("inEmail").value;
+    const senha = document.getElementById("inSenha").value;
+    const confirmaSenha = document.getElementById("inConfirmaSenha").value;
     
+
+    let valido = true;
 
     if (nome === "") {
         document.getElementById("erro-name").textContent = "Erro - Insira um nome completo";
+        valido = false;
     } else {
-        inName.textContent = "";
+        document.getElementById("erro-name").textContent = "";
     }
 
-
+    if (email === "" || !email.includes("@")) {
+        document.getElementById("erro-email").textContent = "Erro - E-mail inválido";
+        valido = false;
+    } else {
+        document.getElementById("erro-email").textContent = "";
+    }
     
     if (senha.length < 6) {
         document.getElementById("erro-senha").textContent = "Erro - Insira no minimo 6 caracteres";
+        valido = false;
+    }else {
+        document.getElementById("erro-senha").textContent = "";
     }
 
+
     if (confirmaSenha !== senha ) {
-        document.getElementById("erro-confrimar").textContent = "Erro - Senhas diferentes";
+        document.getElementById("erro-confirmar").textContent = "Erro - Senhas diferentes";
+        valido = false;
+    } else {
+        document.getElementById("erro-confirmar").textContent = "";
     }
+
+    if (valido) {
+        const divSucesso = document.getElementById('sucesso');
+        const frm = document.getElementById('form');
+
+        divSucesso.classList.remove('oculto');
+
+        frm.classList.add('oculto');
+    }
+
 })
